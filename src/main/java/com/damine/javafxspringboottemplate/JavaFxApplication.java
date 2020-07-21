ce5707c88db87a25c5d9ba3c9cc362a9c770c633
SaadDamine <m.sdaminesaad@gmail.com>
2020-07-21 13:18:31 +0100

    Update JavaFxApplication.java

--- src/main/java/com/damine/javafxspringboottemplate/JavaFxApplication.java
+++ src/main/java/com/damine/javafxspringboottemplate/JavaFxApplication.java
@@ -1,11 +1,29 @@
 package com.damine.javafxspringboottemplate;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 public class JavaFxApplication extends Application {
+    private ConfigurableApplicationContext applicationContext;
     @Override
-    public void start(Stage stage) throws Exception {
-        
+    public void start(Stage stage) {
+
+    }
+
+    @Override
+    public void init() {
+        String[] args = getParameters().getRaw().toArray(new String[0]);
+        this.applicationContext = new SpringApplicationBuilder()
+                .sources(SpringMain.class)
+                .run(args);
+    }
+
+    @Override
+    public void stop() {
+        this.applicationContext.close();
+        Platform.exit();
     }
 }
