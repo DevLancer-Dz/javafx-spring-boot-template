3db9a2e8353a3faa34e47a35f2956a17f997228b
SaadDamine <m.sdaminesaad@gmail.com>
2020-07-21 12:59:19 +0100

    create empty spring boot project

---
+++ src/main/java/com/damine/javafxspringboottemplate/JavafxSpringBootTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.damine.javafxspringboottemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavafxSpringBootTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavafxSpringBootTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/damine/javafxspringboottemplate/JavafxSpringBootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damine.javafxspringboottemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavafxSpringBootTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
