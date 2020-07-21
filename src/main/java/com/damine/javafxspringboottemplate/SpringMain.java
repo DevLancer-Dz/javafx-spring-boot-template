4b22803cee644d58f0f6f954bd5e69012c897887
SaadDamine <m.sdaminesaad@gmail.com>
2020-07-21 13:03:08 +0100

    rename main class

--- src/main/java/com/damine/javafxspringboottemplate/SpringMain.java
+++ src/main/java/com/damine/javafxspringboottemplate/SpringMain.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavafxSpringBootTemplateApplication {
+public class SpringMain {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavafxSpringBootTemplateApplication.class, args);
+        SpringApplication.run(SpringMain.class, args);
     }
 
 }
