06210f576211ae0072265d8e8a5df2bb47bc7d1c
SaadDamine <m.sdaminesaad@gmail.com>
2020-07-21 13:19:03 +0100

    Update SpringMain.java

--- src/main/java/com/damine/javafxspringboottemplate/SpringMain.java
+++ src/main/java/com/damine/javafxspringboottemplate/SpringMain.java
@@ -1,5 +1,6 @@
 package com.damine.javafxspringboottemplate;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringMain {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringMain.class, args);
+        Application.launch(JavaFxApplication.class, args);
     }
 
 }
